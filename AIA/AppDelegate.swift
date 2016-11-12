2d5264d4cc613dca5fd23744a6cc01db41b2f5fd
Ha Lam <halam@LamHa.local>
2016-11-12 12:14:58 +0700

    Init

---
+++ AIA/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  AIA
+//
+//  Created by Ha Lam on 11/12/16.
+//  Copyright © 2016 HTKInc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ AIA/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AIA
+//
+//  Created by Ha Lam on 11/12/16.
+//  Copyright © 2016 HTKInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
