ca5645f305c873c80a641d6b4d6238395b657594
HaLamUs <itzlamz9x@gmail.com>
2016-11-16 00:31:59 +0700

    Add Button

--- AIA/TellUsViewController.swift
+++ AIA/TellUsViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 class TellUsViewController: UIViewController {
+    
+    //MARK: IBOutlets
+    @IBOutlet weak var femaleButton: UIButton!
+    @IBOutlet weak var maleButton: UIButton!
+    
+    @IBOutlet weak var smokerButton: UIButton!
+    @IBOutlet weak var nonSmokerButton: UIButton!
+    
+    //MARK: IBActions
+    @IBAction func touchUpFemaleButton(_ femaleButton: UIButton) {
+        femaleButton.isSelected = !femaleButton.isSelected
+        self.maleButton.isSelected = false
+    }
+    @IBAction func touchUpMaleButton(_ maleButton: UIButton) {
+        maleButton.isSelected = !maleButton.isSelected
+        self.femaleButton.isSelected = false
+    }
+    
+    @IBAction func touchUpSmokerButton(_ smokerButton: UIButton) {
+        smokerButton.isSelected = !smokerButton.isSelected
+        self.nonSmokerButton.isSelected = false
+    }
+    @IBAction func touchUpNonSmokerButton(_ nonSmokerButton: UIButton) {
+        nonSmokerButton.isSelected = !nonSmokerButton.isSelected
+        self.smokerButton.isSelected = false
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
